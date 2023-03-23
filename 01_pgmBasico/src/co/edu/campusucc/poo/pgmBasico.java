42fa744ed7fa680ada82e1ea1be535967759b36a
Luis Fernando <lfcaicedo1987@gnail.com>
2023-03-22 19:52:07 -0500

    Reorgane file java

--- 01_pgmBasico/src/co/edu/campusucc/poo/pgmBasico.java
+++
@@ -1,7 +0,0 @@
-public class pgmBasico {
-    public static void main(String[] args) {
-        System.out.println("Pgm Basico");
-        System.out.println("Luis Fernando Caicedo");
-        System.out.println("Programacion orientado objeto");
-    }
-    }
